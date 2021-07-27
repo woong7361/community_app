2fd022aeb9bcc3ad76d2ab33294617d1c9662448
woong7361 <woong7361@gmail.com>
2021-07-27 11:22:31 +0900

    test

--- src/server.js
+++ src/server.js
@@ -6,12 +6,19 @@ const PORT_NUMBER = process.env.PORT || 5000;
 const app = express();
 app.use(morgan("dev"))
 
-const handeler = (req,res) => {
-    res.redirect('http://google.com')
+
+const handeler1 = (req,res) => {
+    res.send('1');
 }
 
-app.get("/new", handeler);
-app.post("/new",handeler);
+const handeler2 = (req,res) => {
+    res.redirect('http://google.com');
+}
+
+app.get("/", handeler1);
+
+app.get("/new", handeler2);
+app.post("/new",handeler2);
 
 const handleListening = () => {
     console.log(`Server is running on http://localhost:${PORT_NUMBER} `);
